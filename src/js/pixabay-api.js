cfec0067d51f75d1077e812fcbc22cee86707669
Dima <dima@vantage.software>
2024-08-10 18:32:19 +0300

    fix axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '45156920-6e611072a4f9c22ae400d324b';
-const BASE_URL = 'https://pixabay.com/api';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export const fetchImages = async (query, page = 1, perPage = 15) => {
   const params = {
